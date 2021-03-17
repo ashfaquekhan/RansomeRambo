ec70178f06f229749413125593b1a2db94fca70a
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2021-03-17 23:15:49 +0530

    Delete githubread.py

--- githubread.py
+++
@@ -1,6 +0,0 @@
-from github import Github
-github = Github("ashfaquekhan", "f8d66aac85299f6ef03b8a4271a13bcec4f2c04c")
-user = github.get_user()
-repository = user.get_repo("UPDATE1")
-file_content = repository.get_contents("Readme.md")
-print(file_content.decoded_content.decode())
